823f2a135fb4dea373e52ac8b5bb8643f8d5308b
Matthias Strauss <matthias.strauss@mayflower.de>
2020-01-08 14:32:17 +0100

    Improve InputHandler

--- Days/util/InputHandler.cs
+++ Days/util/InputHandler.cs
@@ -8,19 +8,19 @@ namespace ClassLib.util
     {
         public static string[] ReadFile(string fileName)
         {
-            var allLines = new string[1000]; //only allocate memory here
+            var allLines = new List<string>();
             var fileLocation = "../../../../Days/input/";
             using (StreamReader sr = File.OpenText(fileLocation + fileName))
             {
                 int x = 0;
                 while (!sr.EndOfStream)
                 {
-                    allLines[x] = sr.ReadLine();
+                    allLines.Add(sr.ReadLine());
                     x += 1;
                 }
             }
 
-            return allLines;
+            return allLines.ToArray();
         }
     }
 }
